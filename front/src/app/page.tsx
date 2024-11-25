805b0d51385664fe20bd00988926926cd82b0953
Jane <apiwatsupaphol@gmail.com>
2024-11-25 16:51:27 +0700

    feat: use zustand

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,20 +1,48 @@
 "use client";
 import { Button, Card, Image } from "@nextui-org/react";
+import { create } from "zustand";
+
+interface WaterProps {
+  water: number;
+  setWater: (water: number) => void;
+}
+
+interface TempProps {
+  temperature: number;
+  setTemperature: (temp: number) => void;
+}
+
+const useWater = create<WaterProps>((set) => ({
+  water: 1.5,
+  setWater: (value) => set({ water: value }),
+}));
+
+const useTemperature = create<TempProps>((set) => ({
+  temperature: 42.3,
+  setTemperature: (temp) => set({ temperature: temp }),
+}));
 
 export default function Home() {
+  const { water, setWater } = useWater();
+  const { temperature, setTemperature } = useTemperature();
   return (
-    <main className="h-screen w-screen flex  items-center bg-gradient-to-tr from-blue-500 to-blue-50">
-      <section className="max-w-screen-lg mx-auto grid grid-cols-2 gap-4">
+    <main className="h-screen w-screen flex p-4  items-center bg-gradient-to-tr from-blue-500 to-blue-50">
+      <section className="max-w-screen-lg mx-auto grid grid-cols-1 sm:grid-cols-2 gap-4">
         <div>
           <Image src="/images/fishtank.jpg" />
         </div>
 
         <Card shadow="none" className=" p-8 shadow-md">
           <div className=" flex flex-col gap-4">
-            <h1>Temperature: 45 C</h1>
-            <h1>Water level: 45%</h1>
+            <h1>Temperature: {temperature} °C</h1>
+            <h1>Water level: {water}</h1>
             <div>
-              <Button className="  bg-sky-200 ">
+              <Button
+                onPress={() => {
+                  setTemperature(1);
+                }}
+                className="  bg-sky-200 "
+              >
                 <h5>feed</h5>
               </Button>
             </div>

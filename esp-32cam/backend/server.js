c51e00cae92e1ec915d5bc1ffcbf6e993cf97f05
night-sornram <6633236021@student.chula.ac.th>
2024-11-29 15:21:19 +0700

    feat: add a simple Hello World endpoint to the server

--- esp-32cam/backend/server.js
+++ esp-32cam/backend/server.js
@@ -46,6 +46,10 @@ app.post('/upload', async (req, res) => {
     }
 });
 
+app.get("/", (req, res) => {
+    res.send('Hello World')
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 });
